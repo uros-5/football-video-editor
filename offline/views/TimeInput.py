4d9336b052ffdf675739838a59c9230bef46e38f
uros-5 <uros.mrkobrada99@gmail.com>
2021-04-21 19:32:38 +0200

    deleted global counter for TimeInput

--- offline/views/TimeInput.py
+++ offline/views/TimeInput.py
@@ -1,21 +1,18 @@
 from tkinter import Entry,StringVar
 
 class TimeInput(Entry):
-    counter = 0
 
     def __init__(self, master=None, cnf={}, **kw):
         super(TimeInput,self).__init__(master,cnf={},**kw)
         self['textvariable'] = self.set_listener()
-        TimeInput.counter += 1
-        self.set_length()
 
     def unos(self,sv):
         value = sv.get()
-        if len(value) < self.value_length + 1:
+        if len(value) < self['width']+1:
             if not value.isnumeric():
-                sv.set(self.fix_input(value,len(value)))
+                sv.set(self.fix_input(value,len(value)-1))
         else:
-            sv.set(self.fix_input(value,self.value_length))
+            sv.set(self.fix_input(value,self['width']))
     
     def fix_input(self,input,end):
         return input[0:end]
@@ -24,14 +21,3 @@ class TimeInput(Entry):
         sv = StringVar()
         sv.trace('w',lambda name, index, mode, sv=sv: self.unos(sv))
         return sv
-
-    def set_length(self):
-        if TimeInput.counter == 1:
-            self.value_length = 3
-        elif TimeInput.counter == 2:
-            self.value_length = 2
-        elif TimeInput.counter == 3:
-            self.value_length = 2
-            TimeInput.counter = 0
-
-        
